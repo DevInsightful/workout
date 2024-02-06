3dc1584bbc75404eb355e82af1047319f049481d
Zaid <syedzaidahmed21@gmail.com>
2024-02-06 15:08:57 +0500

    dot env added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-import {express} from express
-const app = express()
-const port = 3001
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-app.listen(port,()=>{
-    console.log("Server Succesfully running on",port)
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server Succesfully running on", process.env.PORT);
+});
