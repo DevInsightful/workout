dbc4a44c8e7af0c5842220b4eee61a5d7a959775
Zaid <syedzaidahmed21@gmail.com>
2024-02-07 15:22:53 +0500

    comment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+// this is used to import .env that makes credentials secure
 require("dotenv").config();
 
 const express = require("express");
 
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!" });
 });
 app.listen(process.env.PORT, () => {
   console.log("Server Succesfully running on", process.env.PORT);
