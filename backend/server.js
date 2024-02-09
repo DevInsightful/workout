16eb4b09dfe87946bcc091df70208e6fa8cf737c
Zaid <syedzaidahmed21@gmail.com>
2024-02-09 10:21:09 +0500

    use middleware added

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,16 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+// this use method is the middleware used for managing paths
+app.use((req,res,next)=>{
+  console.log(req.path,req.method)
+  next()
+})
+// A get request to / route
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.send({ message: "Hello World!" });
 });
+// this is to listen all the requests
 app.listen(process.env.PORT, () => {
   console.log("Server Succesfully running on", process.env.PORT);
 });
