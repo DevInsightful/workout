efb7562bb62c6ddc5ed30047fb4140d9be474606
Zaid <syedzaidahmed21@gmail.com>
2024-07-25 15:12:04 +0500

    router added

--- api/server.js
+++ api/server.js
@@ -2,12 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const router = require("./routes/workoutRoutes");
 app.use(cors());
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
   console.log("succesfully running at", process.env.PORT);
 });
-app.get("/", (req, res) => {
-  res.json({ message: "working" });
-});
+app.use(router);
