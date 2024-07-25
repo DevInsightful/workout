b0f4549f1a635bac937c82eb82ceada0704cdf49
Zaid <syedzaidahmed21@gmail.com>
2024-07-25 15:06:01 +0500

    dot env setup and type added

--- api/server.js
+++ api/server.js
@@ -1,5 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-app.listen(3000, () => {
-  console.log("succesfully running at 3000");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log("succesfully running at", process.env.PORT);
+});
+app.get("/", (req, res) => {
+  res.json({ message: "working" });
 });
