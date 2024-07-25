a50e302857ed6c3082fe52a8c140802e7921393b
Zaid <syedzaidahmed21@gmail.com>
2024-07-25 16:05:07 +0500

    middleware added

--- api/routes/workoutRoutes.js
+++ api/routes/workoutRoutes.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const workoutroutes = express.Router();
 
+// get all workout
 workoutroutes.get("/", (req, res) => {
   res.json({ message: "working" });
 });
-workoutroutes.post("/", (req, res) => {
+// get a specific workout
+workoutroutes.get("/:id", (req, res) => {
   res.json({ message: "working" });
+});
+workoutroutes.post("/", (req, res) => {
+  res.json({ message: "added" });
 });
 workoutroutes.patch("/:id", (req, res) => {
   res.json({ message: "updated" });
 });
 workoutroutes.delete("/:id", (req, res) => {
-  res.json({ message: "working" });
+  res.json({ message: "deleted" });
 });
 module.exports = workoutroutes;
