f695bf25ec9fe20bc64e22fcd5f6e18e356b5fbe
Zaid <syedzaidahmed21@gmail.com>
2024-07-25 20:14:57 +0500

    workend

--- api/controllers/workoutController.js
+++ api/controllers/workoutController.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const workout = require("../models/workoutModel.js");
 
-const gellAllWorkouts = (req, res) => {
-  res.json({ message: "get all" });
+const gellAllWorkouts = async (req, res) => {
+  const workouts = await workout.find({});
+  console.log(workouts);
+  res.json({ message: workouts });
 };
 const getById = (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    res.json({ message: `get one ${id}` });
+  const { id } = req.params;
+  console.log(id);
+  res.json({ message: `get one ${id}` });
 };
 module.exports = { gellAllWorkouts, getById };
