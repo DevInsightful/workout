6b02a68f09a13691261c9410c4224080922e71a3
Zaid <syedzaidahmed21@gmail.com>
2024-08-08 16:13:45 +0500

    Email pass auth added

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 const userSchema = mongoose.Schema({
   email: {
     type: String,
@@ -11,7 +12,21 @@ const userSchema = mongoose.Schema({
     required: true,
   },
 });
+const validation = (email, password) => {
+  if (!email || !password) {
+    throw Error("All Fields must be filled");
+  }
+  const isValid = validator.isEmail(email);
+  if (!isValid) {
+    throw Error("Email is not valid");
+  }
+  const strongPassword = validator.isStrongPassword(password);
+  if (!strongPassword) {
+    throw Error("Password is not strong");
+  }
+};
 userSchema.statics.signup = async function (email, password) {
+  validation(email, password);
   const exists = await this.findOne({ email });
   if (exists) {
     throw Error("Email already in use");
@@ -22,6 +37,6 @@ userSchema.statics.signup = async function (email, password) {
 
   const user = await this.create({ email, password: hash });
   return user;
-}; 
+};
 
 module.exports = mongoose.model("User", userSchema);
